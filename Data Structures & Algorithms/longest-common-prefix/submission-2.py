f4cec2481620212eafcf44e11501d6772eb352a8
Saurabh <50540254+Saurabh-think-different@users.noreply.github.com>
2026-09-20 22:48:38 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    # d = [dance, dag, danger, damage]
+    # da = [dance, dag, danger, damage]
+    # dan = [dance, danger]
+    # dag = dag
+    # dam = damage
+
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        lcp = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return lcp
+            lcp += strs[0][i]
+        return lcp
